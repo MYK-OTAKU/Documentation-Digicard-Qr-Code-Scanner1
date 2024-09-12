ba5758a75d798271077bf6bbde3a36a5fc8053ae
MYK-OTAKU <m2017koita@gmail.com>
2024-09-12 04:36:47 +0100

    Test
    dsqdsq

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com/MYK-OTAKU.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Documentation-Digicard-Qr-Code-Scanner/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'Digicard', // Usually your repo name.
+  organizationName: 'MYK-OTAKU', // Usually your GitHub org/user name.
+  projectName: 'Documentation-Digicard-Qr-Code-Scanner', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
