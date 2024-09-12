9c8888e014bd87483bbb9790bbf9310259132991
MYK-OTAKU <m2017koita@gmail.com>
2024-09-12 19:30:08 +0100

    logo img

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,18 +2,8 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
-// List of features with updated GIFs and descriptions for DigiCard
+// Liste des fonctionnalités avec GIFs et descriptions pour DigiCard
 const FeatureList = [
-  {
-    title: 'Facile à Utiliser',
-    Gif: require('@site/static/img/mm.gif').default,
-    description: (
-      <>
-        DigiCard est conçu pour rendre le scan de QR codes intuitif et rapide,
-        avec une interface simple et des fonctionnalités puissantes.
-      </>
-    ),
-  },
   {
     title: 'Analyse de Sécurité Intégrée',
     Gif: require('@site/static/img/se.jpeg').default,
@@ -21,6 +11,16 @@ const FeatureList = [
       <>
         Scannez vos QR codes en toute sécurité. DigiCard utilise l'API VirusTotal
         pour analyser les liens et garantir votre protection.
+      </>
+    ),
+  },
+  {
+    title: 'Facile à Utiliser',
+    Gif: require('@site/static/img/mm.gif').default,
+    description: (
+      <>
+        DigiCard est conçu pour rendre le scan de QR codes intuitif et rapide,
+        avec une interface simple et des fonctionnalités puissantes.
       </>
     ),
   },
@@ -36,12 +36,19 @@ const FeatureList = [
   },
 ];
 
-function Feature({Gif, title, description}) {
+function Feature({ Gif, title, description, idx }) {
+  // Ajoute une classe spéciale pour le premier et le dernier élément
+  const isFirst = idx === 0;
+  const isLast = idx === FeatureList.length - 1;
+  const imageClass = clsx(
+    styles.featureGif, // Classe par défaut pour toutes les images
+    { [styles.specialImage]: isFirst || isLast } // Classe spéciale pour le premier et le dernier
+  );
+
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        {/* Utilisation du GIF au lieu du SVG */}
-        <img src={Gif} className={styles.featureGif} role="img" alt={title} />
+        <img src={Gif} className={imageClass} role="img" alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
@@ -55,9 +62,9 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="row imagess">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <Feature key={idx} idx={idx} {...props} />
           ))}
         </div>
       </div>
